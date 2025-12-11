bfd198fa01addac7f750979b3454e80c18571b95
Alex <alex@ALEKSEIs-MacBook-Pro.local>
2025-12-11 23:31:44 +0300

    Add flash message for label update success

--- server/controllers/labels.js
+++ server/controllers/labels.js
@@ -109,7 +109,9 @@ export const edit = async (req, reply) => {
 
 export const update = async (req, reply) => {
   const { name } = req.body;
+  const t = req.i18next.t.bind(req.i18next);
   await Label.query().patchAndFetchById(req.params.id, { name });
+  req.session.flash = { labels: { success: ['Метка успешно изменена'] } };
   return reply.redirect('/labels');
 };
 
