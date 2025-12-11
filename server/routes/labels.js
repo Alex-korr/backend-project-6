ecd981139227d788e2a9a2a3430f7ab13d6d8ace
Alex <alex@ALEKSEIs-MacBook-Pro.local>
2025-12-11 23:21:05 +0300

    Add POST /labels/:id route for label update (test compatibility)

--- server/routes/labels.js
+++ server/routes/labels.js
@@ -8,6 +8,7 @@ export default async (app) => {
   app.route({ method: 'POST', url: '/labels', handler: create });
   app.route({ method: 'GET', url: '/labels/:id/edit', handler: edit });
   app.route({ method: 'PATCH', url: '/labels/:id', handler: update });
+  app.route({ method: 'POST', url: '/labels/:id', handler: update });
   app.route({ method: 'DELETE', url: '/labels/:id', handler: remove });
   app.route({ method: 'POST', url: '/labels/:id/delete', handler: remove });
 };
